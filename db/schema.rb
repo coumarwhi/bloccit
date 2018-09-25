b48876e2280fa86520d2e2c60f074e09e0593cd6
coumarwhi <coumarwhi@gmail.com>
2018-09-25 11:03:20 -0700

    rails crud assignement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816182624) do
+ActiveRecord::Schema.define(version: 20180917173427) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "copy"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-# #8
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   describe "GET index" do
@@ -12,26 +11,22 @@ RSpec.describe PostsController, type: :controller do
 
     it "assigns [my_post] to @posts" do
       get :index
-# #9
       expect(assigns(:posts)).to eq([my_post])
     end
   end
 
   describe "GET show" do
     it "returns http success" do
-# #16
       get :show, params: { id: my_post.id }
       expect(response).to have_http_status(:success)
     end
     it "renders the #show view" do
-# #17
       get :show, params: { id: my_post.id }
       expect(response).to render_template :show
     end
 
     it "assigns my_post to @post" do
       get :show, params: { id: my_post.id }
-# #18
       expect(assigns(:post)).to eq(my_post)
     end
   end
@@ -42,13 +37,11 @@ describe "GET new" do
     expect(response).to have_http_status(:success)
   end
 
-# #2
   it "renders the #new view" do
     get :new
     expect(response).to render_template :new
   end
 
-# #3
   it "instantiates @post" do
     get :new
     expect(assigns(:post)).not_to be_nil
@@ -56,18 +49,15 @@ describe "GET new" do
 end
 
 describe "POST create" do
-# #4
   it "increases the number of Post by 1" do
     expect{ post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
   end
 
-# #5
   it "assigns the new post to @post" do
     post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
     expect(assigns(:post)).to eq Post.last
   end
 
-# #6
   it "redirects to the new post" do
     post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
     expect(response).to redirect_to Post.last
