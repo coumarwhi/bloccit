b15a17afea900d198655242b8d63953834c142ef
coumarwhi <coumarwhi@gmail.com>
2018-09-28 11:47:11 -0700

    rails auth user model assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
 
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -11,4 +12,14 @@ class User < ApplicationRecord
               length: { minimum: 3, maximum: 254 }
  
     has_secure_password
+
+  def format_name
+    if name
+      name_array = []
+      name.split.each do |n|
+        name_array << n.capitalize
+      end
+      self.name = name_array.join(" ")
+    end 
+  end
 end
