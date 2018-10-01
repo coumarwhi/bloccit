7b0a81efe7217b411a794779b686eb4373978d04
coumarwhi <coumarwhi@gmail.com>
2018-10-01 10:42:01 -0700

    rails auth signing up assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm 
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end 
+
   def create
     @user = User.new
     @user.name = params[:user][:name]
@@ -20,4 +28,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end 
+
+  helper_method :user_params
+  
 end
